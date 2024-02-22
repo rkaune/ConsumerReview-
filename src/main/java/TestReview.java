8be90abb479327640607ffbea7807d4e554b4028
RossICS2022 <119336782+RossICS2022@users.noreply.github.com>
2024-02-22 13:01:23 -0500

    First commit - - -

---
+++ src/main/java/TestReview.java
@@ -0,0 +1,18 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+
+/**
+ *
+ * @author Administrator
+ */
+public class TestReview
+{
+    public static void main(String[] args)
+    {
+        //Test the sentimentVal method here!
+        double num = Review.sentimentVal("sad");
+        System.out.println(num);
+    }
+}
